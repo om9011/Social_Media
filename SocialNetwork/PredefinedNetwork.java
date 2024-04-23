51704c95570716d400d80f1193ffa51b934f34f3
Om-Gujarathi <veryhappyom@gmail.com>
2024-04-23 12:16:31 +0530

    Final Commit

--- SocialNetwork/PredefinedNetwork.java
+++ SocialNetwork/PredefinedNetwork.java
@@ -1,2 +1,21 @@
-package SocialNetwork;public class PredefinedNetwork {
+package SocialNetwork;
+
+import Exceptions.UserNotFoundException;
+
+public class PredefinedNetwork {
+    public static void main(String[] args) throws UserNotFoundException {
+        SocialNetwork2 socialNetwork = new SocialNetwork2();
+
+//      Users Created
+        socialNetwork.addUser("Om Gore");
+        socialNetwork.addUser("Om Gujarathi");
+        socialNetwork.addUser("Sanskar Gundecha");
+        socialNetwork.addUser("Asawari Jadhav");
+
+        socialNetwork.addFriend("Om Gore", "Sanskar Gundecha");
+        socialNetwork.addFriend("Om Gore", "Om Gujarathi");
+
+        socialNetwork.showPosts("Om Gore");
+    }
+
 }
